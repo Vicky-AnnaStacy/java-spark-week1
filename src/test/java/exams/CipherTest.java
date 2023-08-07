1e6900828e3795c708e488a5a2ffe402f948508b
Anastacia Wainaina <37904262+Vicky-AnnaStacy@users.noreply.github.com>
2023-08-07 19:26:31 +0300

    test encrypt

--- src/test/java/exams/CipherTest.java
+++ src/test/java/exams/CipherTest.java
@@ -11,3 +11,16 @@ class CipherTest {
     void setUp() {
         enCryptor = new Cipher();
     }
+}
+
+    @Test
+    void encrypt() {
+        enCryptor.setSteps(23);
+        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", enCryptor.encrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+        enCryptor.setSteps(1);
+        assertEquals("B@#", enCryptor.encrypt("A@#"));
+        assertEquals("", enCryptor.encrypt(""));
+        assertEquals("B", enCryptor.encrypt("B"));
+        assertEquals(null, enCryptor.encrypt(null));
+    }
+
