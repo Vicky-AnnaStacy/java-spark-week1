1db801a0233dd5f8d844232cd1fb71407e238c28
Anastacia Wainaina <37904262+Vicky-AnnaStacy@users.noreply.github.com>
2023-08-07 18:49:23 +0300

    added  output

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -12,3 +12,22 @@ public final class Main {
         Scanner myInputKey = new Scanner(System.in);
         System.out.println("This is a program that encrypts and decrypts data. \n Please select from the options provided");
         System.out.println("=============================================================================================");
+
+        System.out.println("OPTIONS");
+        System.out.println("1. ENCRYPT");
+        System.out.println("2. DECRYT");
+        System.out.println("3. EXIT");
+        boolean exit = false;
+        int option, key = 1;
+        String data, result;
+        Cipher cipher = new Cipher();
+        System.out.println("=============================================================================================");
+        do {
+            System.out.println("Enter an option to continue");
+            System.out.println("=============================================================================================");
+            option = Helper.isValidNumber(myOptionInput.nextLine());
+            if (option == 1 || option == 2) {
+                System.out.println("Please enter key. Key should be a number between 1-25");
+                key = Helper.isValidNumber(myInputKey.nextLine(), key);
+                cipher.setSteps(key);
+            }
