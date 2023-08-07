f5a504ce4057f82aebecdd8d8bda04092a04e5c5
Anastacia Wainaina <37904262+Vicky-AnnaStacy@users.noreply.github.com>
2023-08-07 18:47:53 +0300

    added public class

---
+++ src/main/java/Program.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public final class Main {
+
+    private Main() {
+
+    }
+    public static void main(String[] args) {
+        Scanner myOptionInput = new Scanner(System.in);
+        Scanner myInput = new Scanner(System.in);
+        Scanner myInputKey = new Scanner(System.in);
+        System.out.println("This is a program that encrypts and decrypts data. \n Please select from the options provided");
+        System.out.println("=============================================================================================");
